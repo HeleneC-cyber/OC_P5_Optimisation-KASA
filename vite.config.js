a4b5a4f24953588f4db011457df3ed11ec2b218a
Hélène C. <buritsuki@gmail.com>
2025-01-01 19:47:30 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/déploy-github/",
+  base:"/OC_P5_Optimisation-KASA/",
   plugins: [react()],
 })
